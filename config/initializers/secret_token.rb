1db48fc13a540558762cbe3d0149445f810798ca
CodeMongoose <ksdhanjal@gmail.com>
2014-09-25 17:40:03 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Indulge::Application.config.secret_key_base = '8bf8e9d76d26df768e9ba70daf8d2191017e41552320c8eae99e626a5c315cf94365b33c04a5cc1b4ffe75d791f2fdfea9c61885a23315da6ddd92ffece4cc5c'
