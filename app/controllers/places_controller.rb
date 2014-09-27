d9f03eb1ddee2db9ed2250e971d86f08e410af5a
CodeMongoose <ksdhanjal@gmail.com>
2014-09-27 09:03:20 +0000

    Allow Edit of a PLACE - ch 22 and 23

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create]
 
 	def index
 		@places = Place.all
@@ -19,7 +19,16 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 	end
 
+	def edit
+		@place = Place.find(params[:id])
+	end
 
+	def update
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
+
+	end
 
 	private
 	def place_params
