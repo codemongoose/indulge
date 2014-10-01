a7ad92db33749f0893f8c7af8ad23bff83779c81
CodeMongoose <ksdhanjal@gmail.com>
2014-10-01 17:37:34 +0000

    Enabled Pagination using will_paginate -ch 12

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'simple_form'
 gem 'devise', '>= 3.2.4'
 gem 'geocoder'
 gem "figaro", ">= 1.0.0"
+gem 'will_paginate', '> 3.0'
 
 
 # Use ActiveModel has_secure_password
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,8 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
 	def index
-		@places = Place.all
+		@places = Place.all.page(params[:page]).per_page(5)
+
 	end
 
 	def new
